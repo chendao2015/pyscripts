7121776075a49c5e217564cc19d72e2d5eff98c7
chendao2015 <sean1002@126.com>
2018-02-27 10:28:14 +0800

    Add files via upload

--- mail_send.py
+++ mail_send.py
@@ -1,48 +1,34 @@
-#!/usr/bin/env python3
-# -*-encoding: utf-8 -*-
-
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-def mail():
-    ret = True
-    try:
-        msg = MIMEText(mail_context,'plain','utf-8')
-        msg['From'] = formataddr(['发件人昵称',my_sender])
-        msg['To'] = recv_user
-        msg['Cc'] = cc_user
-        msg['Subject'] = mail_subject
+def connect(smtp_server,user,password,smtp_server_port=25):
+    server = smtplib.SMTP(smtp_server,smtp_server_port)
+    server.login(user,password)
+    return server
 
-        server = smtplib.SMTP(smtp_server,smtp_server_port)
-        server.login(my_sender,my_pass)
-        server.sendmail(my_sender,recv_user.split(',')+cc_user.split(','),msg.as_string())
-        server.quit()
-    except Exception:
-        ret = False
-    return ret
+def send(smtp_server,user,password,subject,context,recv_user='',cc_user='',smtp_server_port=25):
+    '''
+recv_user argument must be string, comma interval, like 'fuxing.cheng@126.com, a@b.com'
+cc_user argument must be string, comma interval, like 'fuxing.cheng@126.com, a@b.com'
+    '''
+    if not recv_user:
+        return 'Error, Please enter a recipient mailbox address.'
 
-if __name__ == '__main__':
-    smtp_server = 'smtp.126.com'
-    smtp_server_port = 25
-    my_sender = 'abc@126.com'   #发件人邮箱
-    my_pass = 'xxooxxoo'    #发件人邮箱密码
-    recv_user = 'a@b.com,c@d.com,'   #收件人
-    cc_user = 'e@f.com,g@j.com,'
+    mail_subject = subject
+    mail_context = context
 
-    mail_subject = '邮箱主题写在这里'    #邮箱主题
+    msg = MIMEText(mail_context,'plain','utf-8')
+    msg['From'] = formataddr(['发件人昵称',user])
+    msg['To'] = recv_user
+    msg['Cc'] = cc_user
+    msg['Subject'] = mail_subject
 
-    mail_context = '''
-这是一个段落
-有没有换行？
-This is a test page.
-haha
-How are you?
-How old are you?
-    '''    #邮箱内容
-
-    ret = mail()
-    if ret:
-        print('邮件发送成功')
+    server = connect(smtp_server,user,password)
+    if not cc_user:
+        server.sendmail(user,recv_user.split(','),msg.as_string())
     else:
-        print('邮件发送失败')
+        server.sendmail(user,recv_user.split(',')+cc_user.split(','),msg.as_string())
+    server.quit()
+
+    return 'Mail delivery success'
